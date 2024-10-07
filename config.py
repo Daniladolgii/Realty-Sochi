a4d41e9acec74c9221518469228574b9dbf41ac1
Daniladolgii <makarov@mak-ko.com>
2024-10-07 19:09:59 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+
+# Настройки для Telegram бота
+BOT_TOKEN = '7756552027:AAENxlhSokNmmUiPpZ9iShGP1RE4tJZUE0o'
+CHANNEL_ID = '@Realty_Sochi_and_UAE_bot'
+
+# URL сайта для скрейпинга
+SCRAPING_URL = 'https://sochi1.ru/text/realty/'
